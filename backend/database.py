5ca45cebe93ff1282664a8033cf2683a4b051485
boneandrea <bone.andrea.anthrax@gmail.com>
2024-02-05 14:40:07 +0900

    change env

--- backend/database.py
+++ backend/database.py
@@ -2,12 +2,16 @@ from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
 
-load_dotenv(override=True)
+load_dotenv()
 
 
 class database:
     def connect_db(self):
-        return MongoClient(os.environ.get('SERVER'),
+        print(os.environ.get("DB_SERVER"))
+        print(os.environ.get("DB_PORT"))
+        print(os.environ.get("DB_USERNAME"))
+        print(os.environ.get("DB_PASSWORD"))
+        return MongoClient(os.environ.get('DB_SERVER'),
                            int(os.environ.get('DB_PORT')),
-                           username=os.environ['USERNAME'],
-                           password=os.environ['PASSWORD'])
+                           username=os.environ['DB_USERNAME'],
+                           password=os.environ['DB_PASSWORD'])
