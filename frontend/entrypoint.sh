0c32235336936553601f49078106b4a5d40408c0
boneandrea <bone.andrea.anthrax@gmail.com>
2024-02-09 11:56:31 +0900

    別のdocker

---
+++ frontend/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -eux
+PORT=8888
+echo "server {
+	listen $PORT default_server;
+	listen [::]:$PORT default_server;
+	root /app/frontend/dist;
+	index index.html;
+	server_name _;
+	location / {
+		try_files \$uri \$uri/ =404;
+	}
+	location /api {
+      proxy_pass http://0.0.0.0:8000;
+	}
+}" >| /etc/nginx/conf.d/default.conf
+cd /app/frontend
+npm i
+npm run build
+
+nginx -g "daemon off;"
