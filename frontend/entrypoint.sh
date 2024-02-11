1744490e29e691d628630ab1389c7731b2f65969
boneandrea <bone.andrea.anthrax@gmail.com>
2024-02-11 23:15:34 +0900

    frontをnpm run devに変更

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -2,19 +2,26 @@
 
 set -eux
 
-PORT=80
+# npm run build
 
-echo "server {
-	listen $PORT default_server;
-	root /app/frontend/dist;
-	index index.html;
-	server_name _;
-	location / {
-		try_files \$uri \$uri/ =404;
-	}
-}" >| /etc/nginx/conf.d/default.conf
-cd /app/frontend
+# PORT=$WEB_PORT
+# PORT=80
+# echo "server {
+# 	listen $PORT default_server;
+# 	root /app/dist;
+# 	index index.html;
+# 	server_name _;
+# 	location / {
+# 		try_files \$uri \$uri/ =404;
+# 	}
+# }" >| /etc/nginx/conf.d/default.conf
+
+#nginx -g "daemon off;"
+
+# sed -i "s/listen\s*80/listen 8888/" /etc/nginx/conf.d/default.conf
+# sed -i '0,/root.*/ s/\/usr\/share\/nginx\/html/\/app\/dist/' /etc/nginx/conf.d/default.conf
+
+# cat /etc/nginx/conf.d/default.conf
+
 npm i
-npm run build
-
-nginx -g "daemon off;"
+npm run dev
