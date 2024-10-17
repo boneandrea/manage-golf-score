ef444924a8531cf499497618df5721585df53f64
boneandrea <bone.andrea.anthrax@gmail.com>
2024-10-17 16:43:12 +0900

    Change links

--- frontend/src/components/Table.vue
+++ frontend/src/components/Table.vue
@@ -230,9 +230,11 @@ const today = new Date()
 <template>
   <div>
     <h1 class="green">スコア編集</h1>
-    <p>
-      <a href="https://boneandrea.github.io/gplus-golf-score/" target="_blank">ランキングページ</a>
-    </p>
+    <ul>
+      <li><a href="https://boneandrea.github.io/gplus-golf-score/2024" target="_blank">2024ランキングページ</a></li>
+      <li><a href="https://boneandrea.github.io/gplus-golf-score/2025" target="_blank">2025ランキングページ</a></li>
+      <li><a href="https://boneandrea.github.io/gplus-golf-score/2026" target="_blank">2026ランキングページ</a></li>
+    </ul>
     <p>
       <a href="./pdf">download</a>
     </p>
