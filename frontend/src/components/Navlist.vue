4fc8e2b319f61988af105e4059fdfc9473c3be0a
boneandrea <bone.andrea.anthrax@gmail.com>
2024-11-17 04:24:13 +0900

    hide datelist on create new item

--- frontend/src/components/Navlist.vue
+++ frontend/src/components/Navlist.vue
@@ -29,6 +29,7 @@ const emit = defineEmits(['receive'])
 const showList = ref(false)
 const dateList = ref([])
 const newScore = (e) => {
+  dateList.value.splice(0)
   emit('receive', {})
 }
 const recv = (data) => {
