d0be7f20941f3f7b3756634bf4fb4f167c0772af
boneandrea <bone.andrea.anthrax@gmail.com>
2024-11-19 06:36:23 +0900

    lint

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,27 +1,25 @@
-export const HOLE=18
-export const getPrize= (par, shot)=> {
-  if(par === null) return null
+export const HOLE = 18
+export const getPrize = (par, shot) => {
+  if (par === null) return null
   if (shot === 1) return 'HOLEINONE'
   const diff = shot - par
 
-
   switch (diff) {
-  case -3:
-    return "ALBATROSS"
-  case -2:
-    return "EAGLE"
-  case -1:
-    return "BIRDIE"
-  case 0:
-    return "PAR"
-  case 1:
-    return "BOGEY"
-  case 2:
-    return "DOUBLEBOGEY"
-  case 3:
-    return "TRIPLEBOGEY"
-  default:
-    return null
+    case -3:
+      return 'ALBATROSS'
+    case -2:
+      return 'EAGLE'
+    case -1:
+      return 'BIRDIE'
+    case 0:
+      return 'PAR'
+    case 1:
+      return 'BOGEY'
+    case 2:
+      return 'DOUBLEBOGEY'
+    case 3:
+      return 'TRIPLEBOGEY'
+    default:
+      return null
   }
-
 }
