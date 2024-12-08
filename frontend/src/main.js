52dbc66d738d8ec1c05896bbc7e096b97e9071e3
Takuya Tanaka <banchou@peixe.biz>
2024-12-09 03:56:43 +0900

    setup router

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,5 +2,18 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createMemoryHistory, createRouter } from 'vue-router'
+import HdcpView from './components/HdcpView.vue'
 
-createApp(App).mount('#app')
+const routes = [
+    { path: '/hdcp', component: HdcpView },
+]
+
+const router = createRouter({
+    history: createMemoryHistory(),
+    routes,
+})
+
+createApp(App)
+    .use(router)
+    .mount('#app')
