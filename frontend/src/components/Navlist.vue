53d4be77e1ab1e481f6133405d21f12ad4f800be
boneandrea <bone.andrea.anthrax@gmail.com>
2024-12-09 09:59:06 +0900

    routerで切り替え可能

--- frontend/src/components/Navlist.vue
+++ frontend/src/components/Navlist.vue
@@ -1,39 +1,40 @@
 <template>
-    <div>
-        <nav class="nav-list-wrapper">
-            <ul class="nav-list">
-                <li class="nav-list-item" @click="newScore"><a href="#">新規入力</a></li>
-                <li class="nav-list-item" @click="editScore">
-                    <a href="#">過去データ修正</a>
-                    <DateList @receive="recv" class="ml-auto" :dateList="dateList"></DateList>
-                </li>
-                <li class="nav-list-item">
-                    <a href="https://boneandrea.github.io/gplus-golf-score/2024" target="_blank">2024</a>
-                </li>
-                <li class="nav-list-item">
-                    <a href="https://boneandrea.github.io/gplus-golf-score/2025" target="_blank">2025</a>
-                </li>
-                <li class="nav-list-item">
-                    <a href="https://boneandrea.github.io/gplus-golf-score/2026" target="_blank">2026</a>
-                </li>
-                <li class="nav-list-item">
-                    <a
-                        href="https://peixe.biz/junk/donguri/update_gplus_tour.php?key=ahv0doodinaefahv8Sahgee4ede1shee"
-                        target="_blank"
-                        @click="updateRanking"
-                    >ランキング更新</a
-                                   >
-                </li>
-                <li class="nav-list-item">
-                    <a href="#">HDCP管理</a>
-                </li>
-                <RouterLink @click="goHdcp" to="/hdcp">HDCP管理</RouterLink>
-                <li class="nav-list-item">
-                    <a href="#" @click="download">download</a>
-                </li>
-            </ul>
-        </nav>
-    </div>
+  <div>
+    <nav class="nav-list-wrapper">
+      <ul class="nav-list">
+        <li class="nav-list-item" @click="newScore">
+          <RouterLink @click="goHdcp" to="/new">新規入力</RouterLink>
+        </li>
+        <li class="nav-list-item" @click="editScore">
+          <a href="#">過去データ修正</a>
+          <DateList @receive="recv" class="ml-auto" :dateList="dateList"></DateList>
+        </li>
+        <li class="nav-list-item">
+          <a href="https://boneandrea.github.io/gplus-golf-score/2024" target="_blank">2024</a>
+        </li>
+        <li class="nav-list-item">
+          <a href="https://boneandrea.github.io/gplus-golf-score/2025" target="_blank">2025</a>
+        </li>
+        <li class="nav-list-item">
+          <a href="https://boneandrea.github.io/gplus-golf-score/2026" target="_blank">2026</a>
+        </li>
+        <li class="nav-list-item">
+          <a
+            href="https://peixe.biz/junk/donguri/update_gplus_tour.php?key=ahv0doodinaefahv8Sahgee4ede1shee"
+            target="_blank"
+            @click="updateRanking"
+            >ランキング更新</a
+          >
+        </li>
+        <li class="nav-list-item">
+          <RouterLink to="/hdcp">HDCP管理</RouterLink>
+        </li>
+        <li class="nav-list-item">
+          <a href="#" @click="download">download</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
 </template>
 <script setup>
 import { ref, computed } from 'vue'
@@ -47,105 +48,104 @@ const emit = defineEmits(['receive'])
 const showList = ref(false)
 const dateList = ref([])
 const updateRanking = (e) => {
-    if (!confirm('ランキングページを更新しますか？')) {
-        e.preventDefault()
-    }
+  if (!confirm('ランキングページを更新しますか？')) {
+    e.preventDefault()
+  }
 }
-
 
 const router = useRouter()
 const route = useRoute()
 
 const search = computed({
-    get() {
-        return route.query.search ?? ''
-    },
-    set(search) {
-        router.replace({ query: { search } })
-    }
+  get() {
+    return route.query.search ?? ''
+  },
+  set(search) {
+    router.replace({ query: { search } })
+  },
 })
 
 const recv = (data) => {
-    // par,古いデータはない
-    if (!data.par) {
-        console.log('set par: default')
-        data.par = [...Array(HOLE)].map((_, i) => null)
-    }
-    console.log(data)
-    emit('receive', data)
+  // par,古いデータはない
+  if (!data.par) {
+    console.log('set par: default')
+    data.par = [...Array(HOLE)].map((_, i) => null)
+  }
+  console.log(data)
+  emit('receive', data)
 }
 const newScore = (e) => {
-    dateList.value.splice(0)
-    emit('receive', {
-        course: '',
-        date: '',
-        par: [...Array(HOLE)].map((_, i) => null),
-        scores: [],
-    })
+  dateList.value.splice(0)
+  emit('receive', {
+    course: '',
+    date: '',
+    par: [...Array(HOLE)].map((_, i) => null),
+    scores: [],
+  })
 }
 const editScore = (e) => {
-    showList.value = !showList.value
-    if (!showList.value) return
+  showList.value = !showList.value
+  if (!showList.value) return
 
-    const apiUrl = `${API_ROOT}/find`
-    fetch(apiUrl, {
-        method: 'GET',
-        headers: { 'content-type': 'application/json' },
+  const apiUrl = `${API_ROOT}/find`
+  fetch(apiUrl, {
+    method: 'GET',
+    headers: { 'content-type': 'application/json' },
+  })
+    .then((response) => {
+      return response.json()
     })
-        .then((response) => {
-            return response.json()
+    .then((data) => {
+      dateList.value.splice(0)
+      data.forEach((d) => {
+        dateList.value.push({
+          date: d.date,
+          id: d._id['$oid'],
+          course: d.course,
         })
-        .then((data) => {
-            dateList.value.splice(0)
-            data.forEach((d) => {
-                dateList.value.push({
-                    date: d.date,
-                    id: d._id['$oid'],
-                    course: d.course,
-                })
-            })
-        })
-        .catch((e) => {
-            console.error(e)
-            alert(e)
-        })
-        .finally(() => {})
+      })
+    })
+    .catch((e) => {
+      console.error(e)
+      alert(e)
+    })
+    .finally(() => {})
 }
 const download = async (e) => {
-    const apiUrl = `${API_ROOT}/download`
-    const response = await fetch(apiUrl, {
-        method: 'GET',
-    })
+  const apiUrl = `${API_ROOT}/download`
+  const response = await fetch(apiUrl, {
+    method: 'GET',
+  })
 
-    if (response.ok) {
-        const blob = await response.blob()
-        const url = window.URL.createObjectURL(blob)
-        const a = document.createElement('a')
-        a.href = url
-        a.download = 'golf.json'
-        a.click()
-    }
+  if (response.ok) {
+    const blob = await response.blob()
+    const url = window.URL.createObjectURL(blob)
+    const a = document.createElement('a')
+    a.href = url
+    a.download = 'golf.json'
+    a.click()
+  }
 }
 </script>
 <style>
 .nav-list-wrapper {
-    padding: 10px;
+  padding: 10px;
 }
 .nav-list-item {
-    list-style: none;
-    padding: 8px 0;
+  list-style: none;
+  padding: 8px 0;
 }
 .nav-list-item a {
-    color: #40b983;
-    text-decoration: none;
-    display: inline-block;
-    width: 100%;
-    font-size: 16px;
-    font-weight: 700;
+  color: #40b983;
+  text-decoration: none;
+  display: inline-block;
+  width: 100%;
+  font-size: 16px;
+  font-weight: 700;
 }
 .nav-list-item a:hover {
-    background: #40b983;
-    opacity: 0.8;
-    color: #fff;
+  background: #40b983;
+  opacity: 0.8;
+  color: #fff;
 }
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,16 +4,24 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { createMemoryHistory, createRouter } from 'vue-router'
 import HdcpView from './components/HdcpView.vue'
+import ScoreFormView from './components/ScoreFormView.vue'
 
 const routes = [
-    { path: '/hdcp', component: HdcpView },
+  {
+    path: '/new',
+    component: ScoreFormView,
+    props: true,
+  },
+  {
+    path: '/hdcp',
+    component: HdcpView,
+    props: true,
+  },
 ]
 
 const router = createRouter({
-    history: createMemoryHistory(),
-    routes,
+  history: createMemoryHistory(),
+  routes,
 })
 
-createApp(App)
-    .use(router)
-    .mount('#app')
+createApp(App).use(router).mount('#app')
